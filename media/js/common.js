223eba19dc2ca1ae3bdee8768e58fe8809fb1904
0x46616c6b <louis@blacksec.org>
2011-01-16 22:54:59 +0100

    First changes for a full ajaxable manage ui

--- media/js/common.js
+++ media/js/common.js
@@ -1,36 +1,84 @@
-	function submitNewEntry() {
-		var newEntry = $("#newEntry").val();
-		
-		if (newEntry == 'Neue Meldung schreiben...') {
-			return false;
-		}
-		
-		if ( $('input:checkbox:checked').val() == 'on' ) {
-			var isPublic = parseInt(1);
-		} else {
-			var isPublic = parseInt(0);
-		}
-		
-		$.ajax({
-			type: "POST",
-			data: { "newEntry" : newEntry, "isPublic" : isPublic },
-			success: function() {
-				$('#newEntry').val('Neue Meldung schreiben...');
-				refreshEntries();
-				var newEntryCount = parseInt($('.entry-counter').html()) + 1;
-				setCounter(newEntryCount);
-				$('.charCounter').html('');
-			}
-		});
-	}
+$(document).ready(function() {
+	getEntries();
 	
-	function setCounter(counter) {
-		if ( counter == 1 ) {
-			$('.entry-counter-box').html('Aktuell <span class="entry-counter">'+ counter +'</span> Meldung');
-		} else {
-			$('.entry-counter-box').html('Aktuell <span class="entry-counter">'+ counter +'</span> Meldungen');
-		}	
-	}
+});
+
+function getEntries(action) {
+	if (action == 'reload')
+		$('.entry-list').empty();
+		
+	$.getJSON('?format=json&function=get_all_entries', function(data) {
+		$(data).each(function(i,entry) {
+			if ( i == null ) return false;
+			var text = entry.fields.text;
+			var isPublic = entry.fields.isPublic;				
+			if (isPublic == true) {
+				var status = 'Öffentlich';
+				var statusCss = 'public';
+			} else {
+				var status = 'Intern';
+				var statusCss = 'private';
+			}
+			var published = entry.fields.published;
+			var id = entry.pk;
+			var entry = '.entry-' + id;
+			// <li class="entry entry-1">Eintrag...<span class="published">...</span><span class="delete"></span></li>
+			$('.entry-list').append('<li class="entry entry-'+id+' ' + statusCss + '"></li>');
+			$(entry).append('<span class="entry-text">' + text + '</span>');
+			$(entry).append('<span class="entry-status">' + status +'</span>');
+			$(entry).append('<span class="entry-published">' + published + '</span>');
+			$(entry).append('<span class="entry-delete"></span>');
+		});
+		
+  	});
+
+	setCounter(action);
+}
+
+
+function setCounter(action) {
+	if (action == 'reload')
+		$('.entry-counter-block').empty();
+		
+	$.get('?format=plain&function=get_count', function(data) {
+		count = data
+		if (count == 0) 
+			var message = 'Keine Einträge gespeichert';
+		else if (count == 1)
+			var message = 'Ein Eintrag gespeichert';
+		else
+			var message = count + ' Einträge gespeichert';
+		$('.entry-counter-block').append(message);	
+	});
+}
+
+
+function submitNewEntry() {
+	var text = $('.new-entry').val();
+		
+	if (text == 'Neuen Eintrag schreiben...')
+		return false;
+		
+	if ( $('.is-public:checkbox:checked').val() == 'on' )
+		var isPublic = parseInt(1);
+	else 
+		var isPublic = parseInt(0);
+	
+	$.ajax({
+		type: "POST",
+		// format: { "text" : "text body", "isPublic" : 0|1 }
+		data: { "entry-text" : text, "entry-is-public" : isPublic, "submit" : "submit", "new_entry" : "new_entry" },
+		success: function() {
+			$('.new-entry').val('Neuen Eintrag schreiben...');
+			getEntries('reload');
+		}
+	});
+}
+
+/*
+ * OLD STUFF
+ *
+ */
 
 	function submitNewPin() {
 		var newPin = $("#newPin").val();
@@ -57,27 +105,6 @@
 		
 		var box = "div.entry-"+id;
 		$(box).hide('slow');
-	}
-	
-	function getEntries() {
-		$.getJSON('?entries.json', function(data) {
-			for (i = 0; i < data.length; i++) {
-				var text = data[i].fields.text;
-				var isPublic = data[i].fields.isPublic;
-				if (isPublic == true) {
-					var status = 'Öffentlich';
-				} else {
-					var status = 'Intern';
-				}
-				var published = data[i].fields.published;
-				var pk = data[i].pk;
-				var container = '.entry-'+ pk;
-				$('.entries-box').append('<div class="entry-box box entry-'+pk+'"></div>');
-				$(container).append('<div class="entry-text">'+text+'<img src="../media/img/deleteButton.png" title="Diesen Eintrag löschen" class="deleteButton" onClick="deleteEntry('+pk+')"/></div>');
-				$(container).append('<div class="entry-status">'+status+'</div>');
-				$(container).append('<div class="entry-date">'+published+'</div>');
-			}
-  		});
 	}
 	
 	function refreshEntries() {
