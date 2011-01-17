4268a90015e2281f160440ce56f607cbc89aa23e
0x46616c6b <louis@blacksec.org>
2011-01-17 01:29:07 +0100

    insert a ajax loader before entries are loaded. some improvements for updating entries and the counter.

--- media/js/common.js
+++ media/js/common.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	getEntries();
+	setTimeout("setCounter()", 1000);
 	setPin();
 });
 
@@ -31,25 +32,56 @@ function getEntries(action) {
 		});
 		
   	});
-
-	setCounter(action);
+	
+	hideLoader();
 }
 
-
-function setCounter(action) {
-	if (action == 'reload')
-		$('.entry-counter-block').empty();
+function updateEntries() {
+	$.getJSON('?format=json&function=get_latest_entry', function(data) {
+		$(data).each(function(i,entry) {
+			if ( i == null ) return false;
+			var text = entry.fields.text;
+			var isPublic = entry.fields.isPublic;				
+			if (isPublic == true) {
+				var status = 'Öffentlich';
+				var statusCss = 'public';
+			} else {
+				var status = 'Intern';
+				var statusCss = 'private';
+			}
+			var published = entry.fields.published;
+			var id = entry.pk;
+			var entry = '.entry-' + id;
+			// <li class="entry entry-1">Eintrag...<span class="published">...</span><span class="delete"></span></li>
+			$('.entry-list').prepend('<li class="entry entry-'+id+' ' + statusCss + '"></li>');
+			$(entry).append('<span class="entry-text">' + text + '</span>');
+			$(entry).append('<span class="entry-status">' + status +'</span>');
+			$(entry).append('<span class="entry-published">' + published + '</span>');
+			$(entry).append('<span class="entry-delete" onclick="deleteEntry('+ id +')">Löschen</span>');
+		});
 		
-	$.get('?format=plain&function=get_count', function(data) {
-		count = data
-		if (count == 0) 
-			var message = 'Keine Einträge gespeichert';
-		else if (count == 1)
-			var message = 'Ein Eintrag gespeichert';
-		else
-			var message = count + ' Einträge gespeichert';
-		$('.entry-counter-block').append(message);	
-	});
+  	});
+  	
+  	updateCounter();
+}
+
+function setCounter() {
+	$('.entry-counter-block').empty();
+	var count = $('.entry').size();
+	
+	if (count == 0) 
+		var message = 'Keine Einträge gespeichert';
+	else if (count == 1)
+		var message = 'Ein Eintrag gespeichert';
+	else
+		var message = count + ' Einträge gespeichert';
+	
+	$('.entry-counter-block').append(message).animate({ "opacity" : 1 }, "fast");	
+}
+
+function updateCounter() {
+	$('.entry-counter-block').animate({ "opacity" : 0}, "slow");
+	setTimeout("setCounter()", 1000);
 }
 
 function setPin(action) {
@@ -58,12 +90,13 @@ function setPin(action) {
 	});
 }
 
-
 function submitNewEntry() {
 	var text = $('.new-entry').val();
 		
-	if (text == 'Neuen Eintrag schreiben...')
+	if (text == 'Neuen Eintrag schreiben...') {
+		showMessageBox('Eintrag unterscheidet sich nicht von dem Standardtext des Eingabefeldes!', "error");
 		return false;
+	}
 		
 	if ( $('.is-public:checkbox:checked').val() == 'on' )
 		var isPublic = 1;
@@ -75,11 +108,16 @@ function submitNewEntry() {
 		// format: { "text" : "text body", "isPublic" : 0|1 }
 		data: { "entry-text" : text, "entry-is-public" : isPublic, "submit" : "submit", "new_entry" : "new_entry" },
 		success: function() {
-			$('.new-entry').val('Neuen Eintrag schreiben...');
-			showMessageBox('Eintrag gespeichert!');
-			getEntries('reload');
+			resetEntryInput();
+			showMessageBox('Eintrag gespeichert!', "notice");
+			updateEntries();
+			updateCounter();
 		}
 	});
+}
+
+function resetEntryInput() {
+	$('.new-entry').val('Neuen Eintrag schreiben...');
 }
  
 function submitNewPin() {
@@ -89,7 +127,7 @@ function submitNewPin() {
 		type: "POST",
 		data: { "new_pin" : true, "submit" : true, "pin-value" : pin },
 		success: function() {
-			showMessageBox('Pin geändert!');
+			showMessageBox('Pin wurde erfolgreich geändert!', "notice");
 			setPin();
 		}
 	})
@@ -101,16 +139,26 @@ function deleteEntry(id) {
 		type: "POST",
 		data: { "del_entry" : true, "submit" : true, "entry-id" : id },
 		success: function() {
-			showMessageBox('Eintrag gelöscht!');
+			showMessageBox('Eintrag gelöscht!', "notice");
 			getEntries('reload');
 			
 		}
 	});
+	
+	updateCounter();
 }
 
-function showMessageBox(message) {
-	$('.message-block').html(message).show('fast');
-	setTimeout("clearMessageBox()", 2000);
+function hideLoader() {
+	$('.loader').hide('fast');
+}
+
+function showMessageBox(message, type) {
+	if (type == 'error')
+		$('.message-block').html(message).show('fast').css({ "color" : "#FF0000" });
+	else
+		$('.message-block').html(message).show('fast');
+		
+	setTimeout("clearMessageBox()", 4000);
 }
 
 function clearMessageBox() {
